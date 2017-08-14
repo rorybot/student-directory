40e9a445d1e6abfd882b247011538692259f091f
rorybot <29726065+rorybot@users.noreply.github.com>
2017-08-14 19:06:10 +0100

    Included interpolated message about student numbers

--- directory.rb
+++ directory.rb
@@ -1,5 +1,7 @@
+#First we introduce the Academy
 puts "The students of Villains Academy"
 puts "-------------"
+#Then the list of students...
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
 puts "Nurse Ratched"
@@ -11,6 +13,6 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11
-puts " great students"
+#Then the number, and how great they are
+student_count = 11
+puts "Overall, we have #{student_count} great students"
