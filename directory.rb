f5323449dac6f2e176a59589fd7d30f7394741f6
rorybot <29726065+rorybot@users.noreply.github.com>
2017-08-15 18:30:40 +0100

    Changed array to a hash and corrected print(students) method to reference hash, and to call down the cohort value for the student names

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 
 students = [
-  "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddy Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :august},
+  {name: "Darth Vader", cohort: :august},
+  {name: "Nurse Ratched", cohort: :august},
+  {name: "Michael Corleone", cohort: :august},
+  {name: "Alex DeLarge", cohort: :august},
+  {name: "The Wicked Witch of the West", cohort: :august},
+  {name: "Terminator", cohort: :august},
+  {name: "Freddy Krueger", cohort: :august},
+  {name: "The Joker", cohort: :august},
+  {name: "Joffrey Baratheon", cohort: :august},
+  {name: "Norman Bates", cohort: :august},
 ]
 #Introduce the Academy
 def print_header
@@ -19,10 +19,10 @@ def print_header
 end
 
 #Then print students from array...
-def print(names)
-  names.each_with_index {|x, index|
+def print(students)
+  students.each_with_index {|student, index|
     indexplus1 = index+1
-    puts "#{indexplus1}: #{x}"}
+    puts "#{indexplus1}: #{student[:name]} (#{student[:cohort]})"}
 end
 #Then the number, and how great they are
 def print_footer(names)
