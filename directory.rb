65143b44d0f2b33d4dd83acd677b697fd74fe889
rorybot <29726065+rorybot@users.noreply.github.com>
2017-08-15 19:37:16 +0100

    (1) Added extra criteria for each student; (2) Included a check for a variable which allows searching by first letter of first name

--- directory.rb
+++ directory.rb
@@ -1,19 +1,19 @@
 
   students = [
-  {name: "Dr. Hannibal Lecter", cohort: :august},
-  {name: "Darth Vader", cohort: :august},
-  {name: "Nurse Ratched", cohort: :august},
-  {name: "Michael Corleone", cohort: :august},
-  {name: "Alex DeLarge", cohort: :august},
-  {name: "The Wicked Witch of the West", cohort: :august},
-  {name: "Terminator", cohort: :august},
-  {name: "Freddy Krueger", cohort: :august},
-  {name: "The Joker", cohort: :august},
-  {name: "Joffrey Baratheon", cohort: :august},
-  {name: "Norman Bates", cohort: :august},
+  {name: "Dr. Hannibal Lecter", cohort: :august, age: 50, hobbies: "Cooking", born: "America"},
+  {name: "Darth Vader", cohort: :august, age: 45, hobbies: "Fencing", born: "Tatooine"},
+  {name: "Nurse Ratched", cohort: :august, age: 42, hobbies: "Nursing", born: "Hospital"},
+  {name: "Michael Corleone", cohort: :august, age: 35, hobbies: "Favouring", born: "da mafia"},
+  {name: "Alex DeLarge", cohort: :august, age: 25, hobbies: "No idea", born: "Huh"},
+  {name: "The Wicked Witch of the West", cohort: :august, age: 145, hobbies: "Wicca", born: "The West"},
+  {name: "Terminator", cohort: :august, age: 30, hobbies: "Biking", born: "the future"},
+  {name: "Freddy Krueger", cohort: :august, age: 111, hobbies: "Dreaming", born: "In yo dreams"},
+  {name: "The Joker", cohort: :august, age: 80, hobbies: "Comedy", born: "Gotham"},
+  {name: "Joffrey Baratheon", cohort: :august, age: 15, hobbies: "Archery", born: "King's Landing"},
+  {name: "Norman Bates", cohort: :august, age: 40, hobbies: "Dancing", born: "New York"},
 ]
 
-
+=begin
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -32,7 +32,7 @@ def input_students
   students
 
 end
-
+=end
 #Introduce the Academy
 def print_header
   puts "The students of Villains Academy"
@@ -40,17 +40,28 @@ def print_header
 end
 
 #Then print students from array...
-def print(students)
-  students.each_with_index {|student, index|
-    indexplus1 = index+1
-    puts "#{indexplus1}: #{student[:name]} (#{student[:cohort]})"}
+def print(students, letter_selection)
+
+    students.each_with_index {|student, index|
+      indexplus1 = index+1
+      if letter_selection != String
+        if student[:name].start_with? letter_selection.upcase
+        puts "#{indexplus1}: #{student[:name]} (#{student[:cohort]} - born in #{student[:born]}, age #{student[:age]} and enjoys #{student[:hobbies]})"
+        end
+      else
+        puts "#{indexplus1}: #{student[:name]} (#{student[:cohort]} - born in #{student[:born]}, age #{student[:age]} and enjoys #{student[:hobbies]})"
+      end
+    }
+
+
+
 end
 #Then the number, and how great they are
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
+#students = input_students
 print_header
-print(students)
+print(students, "m")
 print_footer(students)
